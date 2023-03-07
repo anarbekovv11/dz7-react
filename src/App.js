101e3f7316479a56c05e17b21ece25bbadeca3fb
anarbekovv11 <anarbekovvv11@gamil.com>
2023-03-07 22:56:03 +0600

    homework7

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [open, setOpen] = useState(false)
+
+    const handleClick = () => {
+        setOpen(!open)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{width: '100%', display: 'flex', justifyContent: 'space-around'}}>
+          <div>
+              <button onClick={handleClick} style={open ? {display: 'block'} : {display: 'none'}} className="red">Red</button>
+          </div>
+          <div>
+              <button onClick={handleClick} style={!open ? {display: 'block'} : {display: 'none'}} className="green" >Green</button>
+          </div>
+      </div>
   );
 }
 
